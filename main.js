db58ba1012f02d627d16349d42b09ddcb3396803
feng <feng@bizdev.us>
2017-10-11 13:31:10 -0600

    First commit

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ function changePic (){
 
 //Change the headings text color to blue
 //HINT: it's a class!
+var headings = document.getElementsByClassName('headings')
+for (var i = 0; i < headings.length; i++){
+  headings[i].style.color = 'blue';
+}
 
 
 //Change the button's background-color to green
-
+var button = document.getElementById('button');
+button.style.backgroundColor = 'green';
+console.log(button);
 
 //Change the paragraph font to 'Tahona'
-
+var pChange = document.getElementsByClassName("paragraph")[0];
+pChange.style.fontFamily = "Arial";
 
 //Change the entire documents background color to yellow
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundColor = 'yellow';
